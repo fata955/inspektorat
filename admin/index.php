010371283f53b58517c8ecf6e581e5cc64aef476
fata955 <fatahillah955@gmail.com>
2025-06-02 19:11:12 +0800

    perbaikan1

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<!--begin::Header-->
+<!--end::Header-->
+<!--begin::Sidebar-->
+
+<?php
+session_start();
+
+if (!empty($_SESSION['username']) and !empty($_SESSION['nama'])) {
+  include("../lib/dbh.inc.php");
+
+  define("INDEX", true);
+
+
+  include 'component/header.php';
+  include 'component/navbar.php';
+  include 'route.php';
+  include 'component/footer.php';
+} else {
+
+  header("Location: /inspektorat/admin/login");
+  exit;
+}
+?>
