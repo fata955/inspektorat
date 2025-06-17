f6e5b96d56cef9c3246e9738b17adb9c87a5b644
fata955 <fatahillah955@gmail.com>
2025-06-17 09:49:08 +0800

    perbaikan 7

--- components/navbar.comp.php
+++ components/navbar.comp.php
@@ -15,14 +15,14 @@
     </div>
   </div> -->
   <!-- End Top Bar -->
-<div class="branding text-dark" style="background-image: url('assets/img/kelor.png'); background-size: 20%">
+  <div class="branding text-dark" style="background-image: url('assets/img/kelor.png'); background-size: 20%">
     <div class="container position-relative d-flex align-items-center justify-content-center">
       <a href="/inspektorat" class="logo d-flex align-items-center">
         <img src="assets/img/palu.png" alt="" />
         <h1 class="sitename">Inspektorat Daerah Kota Palu<br /></h1>
       </a>
 
-      
+
     </div>
   </div>
 
